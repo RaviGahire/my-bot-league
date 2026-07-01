4a72db5fa04839f287ffbe3dd9342f719f827e1f
RaviGahire <ravigahire3@gmail.com>
2026-07-01 14:55:58 +0530

    Add : import index.css in main.tsx

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "../app/store";
 import App from "./App";
+import "../styles/index.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>

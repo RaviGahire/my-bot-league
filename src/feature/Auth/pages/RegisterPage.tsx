52a989feb14784d8c4d7c8bebc4d624ffd2fdf5c
RaviGahire <ravigahire3@gmail.com>
2026-07-02 19:37:53 +0530

    Refactor: clean up RegisterPage by removing unused imports and commented code

--- src/feature/Auth/pages/RegisterPage.tsx
+++ src/feature/Auth/pages/RegisterPage.tsx
@@ -1,22 +1,12 @@
 import AuthLayout from "../../../layouts/AuthLayout";
 import AuthCard from "../components/AuthCard";
-// import OtpSection from "../components/OtpSection";
-// import PasswordSection from "../components/PasswordSection";
-// import TermsSection from "../components/TermsSection";
-// import SocialAuth from "../components/SocialAuth";
 import "../../../styles/login.css";
-import useRegister from "../hooks/useRegister";
 import { SignUp } from "./SignUp";
 
 export default function RegisterPage() {
-  const register = useRegister();
-
   return (
     <AuthLayout>
-      <AuthCard
-      // title="Create New Account"
-      // subtitle="Start your journey in BotLeague"
-      >
+      <AuthCard>
         <SignUp />
       </AuthCard>
     </AuthLayout>
